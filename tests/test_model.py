ba007cdba24f3d7f0386436aefe8019bc76c9b26
wpbindt <wesselbindt@gmail.com>
2021-04-13 15:44:12 +0200

    Reproduce threading bug in model tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -41,3 +41,16 @@ def test_is_playing():
     model.stop()
     assert not model.is_playing
     assert fake_observer.updates == 2
+
+
+def test_rapid_start_stop():
+    fake_sound = FakeSound()
+    model = Model(bpm=240, beat=fake_sound)
+
+    model.start()
+    sleep(0.1)
+    model.stop()
+    model.start()
+    sleep(0.41)
+    model.stop()
+    assert fake_sound.calls == 3
