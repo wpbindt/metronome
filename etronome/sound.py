53d2ab00fcaf7a56ea94cca22858b666f595372d
wpbindt <wesselbindt@gmail.com>
2021-04-16 09:03:44 +0200

    Fix mypy issues

--- etronome/sound.py
+++ etronome/sound.py
@@ -1,5 +1,7 @@
+from pathlib import Path
 import subprocess
+from typing import Union
 
 
-def play_sound(sound_file: str) -> None:
+def play_sound(sound_file: Union[str, Path]) -> None:
     subprocess.Popen(['aplay', '-q', sound_file])
