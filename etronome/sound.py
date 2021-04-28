c9f0aa866399654bb8306242a7be6cb84c56349b
wpbindt <wesselbindt@gmail.com>
2021-04-28 19:02:29 +0200

    Stop using Popen when call suffices

--- etronome/sound.py
+++ etronome/sound.py
@@ -4,4 +4,4 @@ from typing import Union
 
 
 def play_sound(sound_file: Union[str, Path]) -> None:
-    subprocess.Popen(['aplay', '-q', sound_file])
+    subprocess.call(['aplay', '-q', sound_file])
